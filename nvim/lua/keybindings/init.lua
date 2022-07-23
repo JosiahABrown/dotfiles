aaffb3600936ebcffa0c92d856a1f24a2ab8df62
Josiah Brown <88345601+JosiahABrown@users.noreply.github.com>
2022-07-22 19:10:06 -0500

    norekeymap -> noremap typo change

--- nvim/lua/keybindings/init.lua
+++ nvim/lua/keybindings/init.lua
@@ -1,9 +1,8 @@
-vim.g.mapleader = ' '
--- map('mode', 'mapping', 'what-its-mapping', 'options')
-local map = vim.api.nvim_set_map
-local opts = { noremap = true, silent = false }
+-- keymap('mode', 'keymapping', 'what-its-keymapping', 'options')
+local keymap = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = false}
 
-map('n', '<F8>', ':TagbarToggle<CR>', opts) -- Toggles Tagbar
-map('n', '<C-f>', ':NERDTreeFocus<CR>', opts) -- Focus on NerdTree
-map('n', '<C-n>', ':NERDTree<CR>', opts) -- Open NerdTree
-map('n', '<C-t>', ':NERDTreeToggle<CR>', opts) -- Toggle NerdTree
+keymap('n', '<F8>', ':TagbarToggle<CR>', opts) -- Toggles Tagbar
+keymap('n', '<C-f>', ':NERDTreeFocus<CR>', opts) -- Focus on NerdTree
+keymap('n', '<C-n>', ':NERDTree<CR>', opts) -- Open NerdTree
+keymap('n', '<C-t>', ':NERDTreeToggle<CR>', opts) -- Toggle NerdTree
